d1d458f688b81e36564a5f349e58cb5f9cd73504
techbless <hw@yunbin.kr>
2020-03-09 23:13:00 +0900

    Add configuration codes for typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
+import "reflect-metadata";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import createConnection from "./config/ormconfig";
 import app from "./app";
 
-const PORT: number = parseInt(process.env.PORT!) || 3000;
-
-app.listen(PORT, err => {
-  if (err) throw err;
-  else console.log("Server Start: Listen on port ", PORT);
-});
+createConnection()
+  .then(async () => {
+    const PORT: number = parseInt(process.env.PORT!) || 3000;
+    app.listen(PORT, err => {
+      if (err) throw err;
+      else console.log("Server Start: Listen on port ", PORT);
+    });
+  })
+  .catch(err => {
+    console.log("TypeORM connection error: ", err);
+  });
