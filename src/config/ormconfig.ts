5a5b55eb3683577d0cd776d051c4974df4562b34
techbless <hw@yunbin.kr>
2020-03-10 11:20:01 +0900

    Change the way to declare entities

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -1,5 +1,10 @@
 import * as typeorm from "typeorm";
 
+/**
+ * Entities should be declared in connection option.
+ */
+import { User } from "../models/entities/user.entity";
+
 const options: typeorm.ConnectionOptions = {
   type: "mysql",
   host: process.env.MYSQL_HOST,
@@ -7,12 +12,11 @@ const options: typeorm.ConnectionOptions = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  //entities: [],
+  entities: [User], // Entities Declaration
   timezone: "+09:00",
   synchronize: true,
   logging: false,
-  charset: "utf8mb4",
-  entities: ["src/models/entities/**/*.ts"]
+  charset: "utf8mb4"
 };
 
 export default async () => {
