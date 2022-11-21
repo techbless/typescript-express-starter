1cd797577d68b1d2792447b6d4597a9e38989459
Chris Chang <yunbin@hansung.ac.kr>
2022-11-21 13:19:45 +0900

    Update sample controller

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -1,12 +1,25 @@
 import { Request, Response } from 'express';
+import AsyncHandled from 'express-safe-async';
+import Controller from './controller';
 
 /**
  * This is a Controller templates, Duplicate this file to use.
  */
-class SampleController {
-  public sample = (req: Request, res: Response) => {
-    // Some codes for models should be here.
-  };
+class SampleController extends Controller {
+  @AsyncHandled // Use [@AsyncHandled] if the method is async
+  public async sample(req: Request, res: Response) {
+    this.assertType(req.body, {
+      username: 'String',
+      password: 'String',
+      info: `{
+        name: String,
+        age: Number | Undefined
+        ...
+      }`,
+    });
+
+    // Write codes here
+  }
 }
 
 export default new SampleController();
